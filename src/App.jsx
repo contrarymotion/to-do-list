84f8817c1dfbdfab755fd1057309f2fb990c8157
Mark Noesen <noesen.mark@gmail.com>
2023-10-02 14:26:06 -0400

    add useState hook, finish basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
-  return "Hi"
+  useState("")
+  return (
+    <>
+      <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+        <button className="btn">Add</button>
+      </div>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
 }
